691655d99fdb3f82aa2c2825811c90133edb1d30
infeng <fzhihao@outlook.com>
2016-10-08 18:18:10 +0800

    complete dev environment

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('atool-build/lib/webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+var conf = {
+  filename: 'index.html',
+  template: './src/entry/index.html', 
+  inject: true,
+  minify: {
+      removeComments: true,
+      collapseWhitespace: false
+  },
+  hash: true,
+}
+
+module.exports = function(webpackConfig) {
+  webpackConfig.output.publicPath = '/';
+
+  webpackConfig.module.loaders.forEach(function(loader, index) {
+    if(loader.test.toString().indexOf('html') > 0) {
+      loader.loader = 'html';
+    }
+  });
+
+  webpackConfig.plugins.push(
+    new HtmlWebpackPlugin(conf)
+  );
+
+  webpackConfig.plugins.some(function(plugin, i){
+    if(plugin instanceof webpack.optimize.CommonsChunkPlugin) {
+      webpackConfig.plugins.splice(i, 1);
+
+      return true;
+    }
+  });
+
+  return webpackConfig;
+};
