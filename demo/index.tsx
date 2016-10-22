457c7f5abce58dd321db0de1ce116ac23475e447
infeng <fzhihao@outlook.com>
2016-10-22 14:01:00 +0800

    update docs

--- demo/index.tsx
+++ demo/index.tsx
@@ -4,6 +4,8 @@ import Viewer from '../src/Viewer';
 const img2 = require('./images/landscape2.jpg');
 const img = require('./images/landscape.jpg');
 const img3 = require('./images/tibet-6.jpg');
+const forkImg = require('./images/fork_me_ribbon.svg');
+import './index.less';
 
 class App extends React.Component<any, any> {
   constructor() {
@@ -11,33 +13,62 @@ class App extends React.Component<any, any> {
 
     this.state = {
       visible: false,
+      activeIndex: 0,
     };
   }
 
   render() {
+    let images = [{
+      src: img,
+      alt: 'lake',
+    }, {
+      src: img2,
+      alt: 'mountain',
+    }, {
+      src: img3,
+      alt: '',
+    }];
+
     return (
       <div>
-        <button onClick={() => { this.setState({ visible: !this.state.visible }); } }>show</button>
-        <div id="container" style={{
-          width: '500px',
-          height: '500px',
-        }}>
+        <nav className="navbar navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-brand">
+              <a>react-viewer</a>
+            </div>
+          </div>
+        </nav>
+        <a href="https://github.com/infeng/react-viewer">
+          <img className="img-fork" src={forkImg} />
+        </a>
+        <div className="container">
+          <h1 className="title">Demo</h1>
+          <div className="img-list">
+            {images.map((item, index) => {
+              return (
+                <div key={index.toString()} className="img-item">
+                  <img src={item.src} onClick={() => {
+                    this.setState({
+                      visible: true,
+                      activeIndex: index,
+                    });
+                  }}/>
+                </div>
+              );
+            })}
+          </div>
+          <Viewer
+          visible={this.state.visible}
+          onClose={() => { this.setState({ visible: false }); } }
+          images={images}
+          activeIndex={this.state.activeIndex}
+          />
         </div>
-        <Viewer
-        visible={this.state.visible}
-        onClose={() => { this.setState({ visible: false }); } }
-        images={[{
-          src: img,
-          alt: 'lake',
-        }, {
-          src: img2,
-          alt: 'mountain',
-        }, {
-          src: img3,
-          alt: '',
-        }]}
-        container={document.getElementById('container')}
-        />
+        <div className="footer">
+          <div className="container-fluid container-footer">
+            <a href="https://github.com/infeng" className="signature">@infeng</a>
+          </div>
+        </div>
       </div>
     );
   }
