59aaa95068df62a65a85b088cb7fce589164322f
infeng <fzhihao@outlook.com>
2019-10-22 15:51:25 +0800

    website: new design

--- demo/index.tsx
+++ demo/index.tsx
@@ -5,10 +5,9 @@ const img2 = require('./images/landscape2.jpg');
 const img = require('./images/landscape.jpg');
 const img3 = require('./images/tibet-6.jpg');
 const img4 = require('./images/image4.jpg');
-const forkImg = require('./images/fork_me_ribbon.svg');
 import './index.less';
 import classNames from 'classnames';
-import { Row, Col, Button } from 'antd';
+import { Button, List, Checkbox } from 'antd';
 const ButtonGroup = Button.Group;
 
 interface State {
@@ -16,7 +15,84 @@ interface State {
   activeIndex: number;
   mode: 'modal' | 'inline';
   drawerVisible: boolean;
+  drag: boolean;
+  attribute: boolean;
 }
+
+interface OptionData {
+  key: string;
+  type: 'boolean';
+  value?: any;
+}
+
+const optionData: OptionData[] = [{
+  key: 'drag',
+  type: 'boolean',
+}, {
+  key: 'attribute',
+  type: 'boolean',
+}, {
+  key: 'zoomable',
+  type: 'boolean',
+}, {
+  key: 'rotatable',
+  type: 'boolean',
+}, {
+  key: 'scalable',
+  type: 'boolean',
+}, {
+  key: 'downloadable',
+  type: 'boolean',
+}, {
+  key: 'loop',
+  type: 'boolean',
+}, {
+  key: 'noClose',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noImgDetails',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noNavbar',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noToolbar',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noFooter',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'changeable',
+  type: 'boolean',
+}, {
+  key: 'disableKeyboardSupport',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noResetZoomAfterChange',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'noLimitInitializationSize',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'disableMouseZoom',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'downloadInNewWindow',
+  type: 'boolean',
+  value: false,
+}, {
+  key: 'showTotal',
+  type: 'boolean',
+}];
 
 class App extends React.Component<any, Partial<State>> {
   container: HTMLDivElement;
@@ -29,6 +105,13 @@ class App extends React.Component<any, Partial<State>> {
       activeIndex: 0,
       mode: 'modal',
     };
+    optionData.forEach(item => {
+      if (item.value === undefined) {
+        this.state[item.key] = true;
+      } else {
+        this.state[item.key] = item.value;
+      }
+    });
   }
 
   handleChangeModal = (e) => {
@@ -41,6 +124,12 @@ class App extends React.Component<any, Partial<State>> {
     this.setState({
       mode: 'inline',
       visible: true,
+    });
+  }
+
+  handleOption = key => {
+    this.setState({
+      [key]: !this.state[key],
     });
   }
 
@@ -73,24 +162,41 @@ class App extends React.Component<any, Partial<State>> {
       hide: this.state.visible && inline,
     });
 
+    let options = {};
+    optionData.forEach(item => {
+      options[item.key] = this.state[item.key];
+    });
+
     return (
       <div>
-        <nav className="navbar navbar-fixed-top">
+        <nav className="navbar">
           <div className="container-fluid">
             <div className="navbar-brand">
-              <p>react-viewer</p>
+              react-viewer
+            </div>
+            <a className="bagde" href="https://npmjs.org/package/react-viewer">
+              <img src="https://d25lcipzij17d.cloudfront.net/badge.svg?id=js&type=6&v=3.0.0&x2=0" />
+            </a>
+            <a className="bagde" href="https://travis-ci.org/infeng/react-viewer">
+              <img src="https://travis-ci.org/infeng/react-viewer.svg?branch=master" />
+            </a>
+            <a className="bagde" href="https://codecov.io/gh/infeng/react-viewer">
+              <img src="https://codecov.io/gh/infeng/react-viewer/branch/master/graph/badge.svg" />
+            </a>
+            <div className="github">
+              <a className="bagde" href="https://github.com/infeng/react-viewer">
+                <img
+                src="https://img.shields.io/github/stars/infeng/react-viewer?style=social"
+                />
+              </a>
             </div>
           </div>
         </nav>
-        <a href="https://github.com/infeng/react-viewer">
-          <img className="img-fork" src={forkImg} />
-        </a>
         <div className="container">
-          <h1 className="title">Demo</h1>
-          <Row>
-            <Col span={6}>
+          <div className="wrap">
+            <div>
               <h2>Options</h2>
-              <div className="options-list">
+              <div className="options">
                 <ButtonGroup>
                   <Button
                     type={inline ? null : 'primary'}
@@ -105,9 +211,36 @@ class App extends React.Component<any, Partial<State>> {
                       Inline mode
                   </Button>
                 </ButtonGroup>
+                <List
+                  className="options-list"
+                  bordered
+                  dataSource={optionData}
+                  renderItem={item => {
+                    let content = null;
+                    switch (item.type) {
+                      case 'boolean':
+                        content = (
+                          <Checkbox
+                            checked={this.state[item.key]}
+                            onChange={() => { this.handleOption(item.key); }}
+                          >
+                            {item.key}
+                          </Checkbox>
+                        );
+                        break;
+                      default:
+                        break;
+                    }
+                    return (
+                      <List.Item>
+                        {content}
+                      </List.Item>
+                    );
+                  }}
+                />
               </div>
-            </Col>
-            <Col span={12}>
+            </div>
+            <div className="img-list-wrap">
               <div className={imgListClass}>
                 {images.map((item, index) => {
                   return (
@@ -123,8 +256,8 @@ class App extends React.Component<any, Partial<State>> {
                 })}
               </div>
               <div className={inlineContainerClass} ref={ref => {this.container = ref; }}></div>
-            </Col>
-          </Row>
+            </div>
+          </div>
           <Viewer
           visible={this.state.visible}
           onClose={() => {
@@ -143,10 +276,11 @@ class App extends React.Component<any, Partial<State>> {
               },
             }]);
           }}
+          {...options}
           />
         </div>
         <div className="footer">
-          <div className="container-fluid container-footer">
+          <div className="container-footer">
             <a href="https://github.com/infeng" className="signature">@infeng</a>
           </div>
         </div>
