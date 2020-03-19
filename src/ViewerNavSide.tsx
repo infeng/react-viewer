71ac029160e4456bafa90e80d09c138303d9db64
Ciro <ciro.dorsa@cadmus.com.br>
2020-03-19 16:37:50 -0300

    Botao de expandir e recolher miniaturas

--- src/ViewerNavSide.tsx
+++ src/ViewerNavSide.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { ImageDecorator } from './ViewerProps';
-
+import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 export interface ViewerNavSideProps {
   prefixCls: string;
   images: ImageDecorator[];
   activeIndex: number;
   showPaginator: boolean;
+  showToggleNav: boolean;
   onChangeImg: (index: number) => void;
 }
 const pxChange = -30;
@@ -14,6 +15,19 @@ export default class ViewerNavSide extends React.Component<ViewerNavSideProps, a
   static defaultProps = {
     activeIndex: 0,
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true,
+    };
+
+    this.toggleNavVisible = this.toggleNavVisible.bind(this);
+  }
+
+  toggleNavVisible = () => {
+    this.setState({isVisible: !this.state.isVisible});
+  }
 
   handleChangeImg = (newIndex) => {
     if (this.props.activeIndex === newIndex) {
@@ -41,21 +55,32 @@ export default class ViewerNavSide extends React.Component<ViewerNavSideProps, a
     }
 
     return (
-      <div className={`${this.props.prefixCls}-navbarside`}>
-        <ul className={`${this.props.prefixCls}-list ${this.props.prefixCls}-list-transition`} style={listStyle}>
-          {this.props.images.map((item, index) =>
-            <li
-            key={index}
-            className={index === this.props.activeIndex ? 'active' : ''}
-            onClick={() => { this.handleChangeImg(index); }}
-            >
-              <img src={item.src} alt={item.alt} title={item.alt} />
-            </li>
-            )
+      <React.Fragment>
+        <div className={`${this.props.prefixCls}-navbarside`}>
+          {!!this.props.showToggleNav &&
+            <button onClick={this.toggleNavVisible} className={`${this.props.prefixCls}-btn-toggle`}>
+              {!!this.state.isVisible ? <FaAngleRight /> : <FaAngleLeft />}
+            </button>
           }
-        </ul>
-        {paginator}
-      </div>
+          {this.state.isVisible &&
+            <div>
+              <ul className={`${this.props.prefixCls}-list ${this.props.prefixCls}-list-transition`} style={listStyle}>
+                {this.props.images.map((item, index) =>
+                  <li
+                  key={index}
+                  className={index === this.props.activeIndex ? 'active' : ''}
+                  onClick={() => { this.handleChangeImg(index); }}
+                  >
+                    <img src={item.src} alt={item.alt} title={item.alt} />
+                  </li>
+                  )
+                }
+              </ul>
+              {paginator}
+            </div>
+          }
+        </div>
+      </React.Fragment>
     );
   }
 }
